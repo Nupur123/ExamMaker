c0523c5e036591a459d50e5a97d24671dea54e11
jalex26 <janry.alex@robertsoncollege.net>
2014-11-28 14:17:49 -0600

    commit

--- ExamMaker/MainWindow.xaml.cs
+++ ExamMaker/MainWindow.xaml.cs
@@ -1191,6 +1191,12 @@ namespace ExamMaker
                     //lbCorrectAnswers.Items.RemoveAt(lbCorrectAnswers.SelectedIndex);
                     lbCorrectAnswers.Items.Add(txtOptionFillin.Text);
                 }
+                if (this.lbOtherOptions.SelectedIndex >= 0)
+                {
+                    lbOtherOptions.Items.Remove(FillInOptionOld);
+                    //lbCorrectAnswers.Items.RemoveAt(lbCorrectAnswers.SelectedIndex);
+                    lbOtherOptions.Items.Add(txtOptionFillin.Text);
+                }
             }
             txtOptionFillin.Visibility = System.Windows.Visibility.Hidden;
 
@@ -1205,6 +1211,16 @@ namespace ExamMaker
                 txtOptionFillin.Text = lbCorrectAnswers.SelectedItem.ToString();
             }
 
+        }
+
+        private void lbOtherOptions_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbOtherOptions.SelectedIndex != -1)
+            {
+                txtOptionFillin.Visibility = System.Windows.Visibility.Visible;
+                FillInOptionOld = lbOtherOptions.SelectedItem.ToString();
+                txtOptionFillin.Text = lbOtherOptions.SelectedItem.ToString();
+            }
         }
     }
 }
